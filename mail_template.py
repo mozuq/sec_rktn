d20af1b4b33a2daaa7ae6f5ae17e23879cdaeeef
mozuq wa tabun neko <84134652+mozuq@users.noreply.github.com>
2021-05-14 00:47:00 +0900

    Create mail_template.py

---
+++ mail_template.py
@@ -0,0 +1,80 @@
+<html>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta http-equiv="content-style-type" content="text/css">
+    <meta http-equiv="content-language" content="ja">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="noindex">
+    <title>損益表</title>
+    <style type="text/css">
+        html {
+            width: 100%;
+            height: 100%;
+        }
+
+        body,
+        body-class {
+            font-family: Arial, Helvetica, sans-serif;
+        }
+
+        @media only screen and (max-width: 600px) {
+            table {
+                width: 100% !important;
+                min-width: 100% !important;
+            }
+
+            .responsive-td {
+                width: 100% !important;
+                padding-left: 5px !important;
+                padding-right: 5px !important;
+                display: block !important;
+                box-sizing: border-box !important;
+            }
+
+            .responsive-img {
+                width: 100% !important;
+                height: auto !important;
+            }
+        }
+    </style>
+</head>
+
+<body style="width:100%;height:100%;margin:0;padding:0;">
+    <table width="100%" border="0" cellpadding="0" cellspacing="0" style="width:100%;">
+        <tr>
+            <td valign="top">
+                <table width="600" align="center" border="0" cellpadding="0" cellspacing="0""
+                    style="width:600px;">
+                    <tr>
+                        <td>
+                            <table width="100%" border="0" cellpadding="5" cellspacing="0">
+                                <tr>
+                                    <td class="responsive-td" valign="top" align="right" <font size="1"
+                                        style="font-size:9px;line-height:1.4;">
+                                        <a href="http://*******/log.txt" style="text-decoration:underline;">サーバログ{1}</a></font>
+                                    </td>
+                                </tr>
+                            </table>
+                            <table width="100%" border="1" cellpadding="5" cellspacing="0"
+                                style="font-size:8px;line-height:1.4;">
+                                <tr>
+                                    <th>資産</th>
+                                    <th>銘柄</th>
+                                    <th>時価評価額</th>
+                                    <th>評価損益</th>
+                                    <th>前日比</th>
+                                    <th>chart</th>
+                                </tr>
+                                {0}
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    </table>
+</body>
+
+</html>
